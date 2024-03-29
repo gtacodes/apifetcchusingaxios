bb392012689d25413440f19a2715179ef749d480
gtacodes <ipeclab242@outlook.com>
2024-03-29 11:33:50 +0530

    created a axios using node

---
+++ index.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const getApi=async()=>{
+    try {
+        const response=await axios.get('https://www.boredapi.com/api/activity');
+        console.log(response.data);
+    } catch (error) {
+        console.log(error)
+    }
+}
+getApi()
